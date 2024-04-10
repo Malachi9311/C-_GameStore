fb019ac8032e3eb9e6e1cc85dc69b09f4a9bcdcb
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-04-10 23:18:37 +0200

    Game summary C# injection

--- Frontend/Models/GameSummary.cs
+++ Frontend/Models/GameSummary.cs
@@ -5,7 +5,7 @@ public class GameSummary
     public int Id { get; set; }
     public required string Name { get; set; }
     public required string Genre { get; set; }
-    public decimal price { get; set; }
+    public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
 
 }
