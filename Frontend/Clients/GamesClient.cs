47f72ebbff046183a056a271c420d6e726e9e98c
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-04-11 15:26:03 +0200

    form

---
+++ Frontend/Clients/GamesClient.cs
@@ -0,0 +1,39 @@
+﻿using Frontend.Models;
+namespace Frontend.Clients;
+
+public class GamesClient
+{
+    private readonly List<GameSummary> games =
+    [
+    new(){
+Id = 1,
+Name = "Street Fighter II",
+Genre = "Action",
+Price = 19.99M,
+ReleaseDate = new DateOnly(1992, 7, 15)
+},
+new(){
+Id = 2,
+Name = "King of Fighters 03",
+Genre = "Action",
+Price = 29.99M,
+ReleaseDate = new DateOnly(2003, 1, 12)
+},
+new(){
+Id = 3,
+Name = "Hitman: Codename 47",
+Genre = "Adventure",
+Price = 31.99M,
+ReleaseDate = new DateOnly(1999, 4, 5)
+},
+new(){
+Id = 4,
+Name = "fifa 16",
+Genre = "Sport",
+Price = 49.99M,
+ReleaseDate = new DateOnly(2016, 8, 16)
+}
+];
+
+    public GameSummary[] GetGames() => games.ToArray();
+}
--- Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d98df8884dcfc82172bdd04f2bd755ecc7060e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e818ae4c46b50dae44e5a54ff3623236c74ca4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
